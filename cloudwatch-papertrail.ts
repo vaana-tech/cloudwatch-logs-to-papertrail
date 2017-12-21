1d8864e013dde02491a5f8a92e0bbb7ae1ca890c
Sam Gronblom <sam.gronblom@reaktor.co.jp>
2017-12-21 15:17:22 +0900

    Remove warning for non-TSV messages
    - Lambda itself will log rows that don't match this pattern as part of
    normal operation: eg. "START RequestId:
    6adea095-e601-11e7-bbcd-c346de6c314d Version: $LATEST"
    - It would spam up the log for this Lambda to print a warning for each
    of these

--- cloudwatch-papertrail.ts
+++ cloudwatch-papertrail.ts
@@ -50,11 +50,10 @@ function getEnvVarOrFail(varName: string): string {
 const logLevelRegex = /^\[(\w+)\]/
 
 export function parseLogLevel(tsvMessage: string): string | undefined {
-  // The message is a tab separated value string of three columns:
+  // Messages logged manually are tab separated value strings of three columns:
   // date string (ISO8601), request ID, log message
   const messageColumns = tsvMessage.split("\t")
   if (messageColumns.length != 3) {
-    console.warn('Got message with unexpected number of TSV columns:', tsvMessage)
     return undefined
   }
   const message = messageColumns[2]
