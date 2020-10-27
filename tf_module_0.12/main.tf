81da44f31530909ae891a582b7a3793952f43e3b
Jacopo Bufalino <jacopo.bufalino@vaana.fi>
2020-10-27 10:07:39 +0100

    Resolve deprecation warnings

--- tf_module_0.12/main.tf
+++ tf_module_0.12/main.tf
@@ -5,33 +5,33 @@ locals {
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.papertrail.function_name}"
+  function_name = aws_lambda_function.papertrail.function_name
   principal     = "logs.amazonaws.com"
 }
 
 resource "aws_cloudwatch_log_subscription_filter" "all_logs" {
-  count           = "${length(var.monitor_log_group_names)}"
+  count           = length(var.monitor_log_group_names)
   name            = "${var.monitor_log_group_names[count.index]}-papertrail-subscription"
-  destination_arn = "${aws_lambda_function.papertrail.arn}"
-  log_group_name  = "${var.monitor_log_group_names[count.index]}"
-  filter_pattern  = "${var.filter_pattern}"
+  destination_arn = aws_lambda_function.papertrail.arn
+  log_group_name  = var.monitor_log_group_names[count.index]
+  filter_pattern  = var.filter_pattern
 }
 
 resource "aws_lambda_function" "papertrail" {
-  filename         = "${local.lambda_zip_file}"
+  filename         = local.lambda_zip_file
   function_name    = "${var.lambda_name_prefix}-papertrail-lambda"
   handler          = "cloudwatch-papertrail.handler"
-  role             = "${var.lambda_log_role_arn}"
+  role             = var.lambda_log_role_arn
   description      = "Receives events from CloudWatch log groups and sends them to Papertrail"
   runtime          = "nodejs12.x"
-  timeout          = "${var.timeout}"
-  source_code_hash = "${filebase64sha256(local.lambda_zip_file)}"
+  timeout          = var.timeout
+  source_code_hash = filebase64sha256(local.lambda_zip_file)
 
   environment {
     variables = {
-      PAPERTRAIL_HOST  = "${var.papertrail_host}"
-      PAPERTRAIL_PORT  = "${var.papertrail_port}"
-      PARSE_LOG_LEVELS = "${var.parse_log_levels}"
+      PAPERTRAIL_HOST  = var.papertrail_host
+      PAPERTRAIL_PORT  = var.papertrail_port
+      PARSE_LOG_LEVELS = var.parse_log_levels
     }
   }
 
@@ -40,6 +40,6 @@ resource "aws_lambda_function" "papertrail" {
     # because Terraform downloads this module to folder with a random string.
     # However, this causes problems when deploying the Lambda in some situatons,
     # so at the moment we cannot add it.
-    ignore_changes = ["last_modified"]
+    ignore_changes = [last_modified]
   }
 }
